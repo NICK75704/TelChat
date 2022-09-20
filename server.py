8de47818f7912024f9b5219c756fcba97a21efab
NICK75704 <49577608+NICK75704@users.noreply.github.com>
2022-09-20 15:56:50 -0400

    added leaving and joining messages

--- server.py
+++ server.py
@@ -14,7 +14,13 @@ clients = []
 welcome = "Hello!\nTo exit, type 'control' + ']'\nto send a message, type your message followed by the enter key\nClients connected: "
 newClientStr = f"\na new client has connected, there are "
 
-def disconnect(conn):
+def disconnect(conn, clients, clientCount):
+  for client in clients:
+    if client != conn:
+      with open("log.txt") as rl:
+        reply = str(str("client disconnected " + clientCount + " clients connected"))
+        client.sendall(bytes(reply, "ascii"))
+        print("message sent!")
   clients.remove(conn)
   conn.close
 
@@ -53,16 +59,20 @@ def connection(conn, addr, clients, newClientStr, welcome):
     print(f"Incoming connection from {addr}")
     newClient(newClientStr, clients, welcome, addr)
     while True:
+      clientCount = str(len(clients))
       input = conn.recv(1024)
       message = str(input, "ascii")
       timestamp = datetime.datetime.now()
       print("*" + str(bytes(input)) + "*")
       try:
+        if input == bytes(b''):
+          disconnect(conn, clients, clientCount)
+          break
         if input != bytes(b'\r\n') and input != bytes(b'\x1b[A\r\n') and input != bytes(b'\x1b[B\r\n') and input != bytes(b'\x1b[C\r\n') and input != bytes(b'\x1b[D\r\n'):
           print(message)
           if input == bytes(b'exit\r\n'):
             print("bruh")
-            disconnect(conn)
+            disconnect(conn, clients, clientCount)
             break
           if message.startswith("/nickname"):
             nickname = message[10:].replace("\r\n", "")
